9459f1131bc6f95407c8f7443b0e0deeb8c0171e
Pascal Hartig <passy@twitter.com>
2014-06-14 15:41:49 +0100

    Make it work.
    And only that. No public API, lots of repetition. Wonderful base for refactoring
    this into something reusable and composable.

--- Main.hs
+++ Main.hs
@@ -3,6 +3,52 @@
 module Main where
 
 import Prelude
+-- TODO: Qualify imports when exploration phase is over. :)
+import System.Environment
+import Text.XML.HXT.Core
+import Text.HandsomeSoup
+import Data.Tree.NTree.TypeDefs
 
+main :: IO ()
+main = do
+    args <- getArgs
+    html <- readFile $ head args
+    let doc = readString [withParseHTML yes, withWarnings no] html
 
-main = print "Hi"
+    putStrLn "== Styles =="
+    -- TODO: This should look like doc >>> stylesheets >>> getAttrValue ...
+    links <- runX $ extractStylesheets doc
+    print links
+
+    putStrLn "== Scripts =="
+    scripts <- runX $ extractScripts doc
+    print scripts
+
+    putStrLn "== Imports =="
+    imports <- runX $ extractImports doc
+    print imports
+
+-- TODO:
+-- All of these extractors should just be arrows. Should be fun refactoring
+-- this.
+
+extractStylesheets ::
+    ArrowXml cat =>
+    cat a (Data.Tree.NTree.TypeDefs.NTree XNode) ->
+    cat a String
+extractStylesheets doc =
+    doc >>> css "link" >>> hasAttrValue "rel" (== "stylesheet") >>> getAttrValue "href"
+
+extractScripts ::
+    ArrowXml cat =>
+    cat a (Data.Tree.NTree.TypeDefs.NTree XNode) ->
+    cat a String
+extractScripts doc =
+    doc >>> css "script" >>> getAttrValue "src"
+
+extractImports ::
+    ArrowXml cat =>
+    cat a (Data.Tree.NTree.TypeDefs.NTree XNode) ->
+    cat a String
+extractImports doc =
+    doc >>> css "link" >>> hasAttrValue "rel" (== "import") >>> getAttrValue "href"
